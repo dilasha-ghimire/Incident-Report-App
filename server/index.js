e114c63e0a73acdf5172bb25a52f3f147cd570dc
Dilasha Ghimire <dilashhaa@gmail.com>
2025-07-29 15:40:41 +0545

    update: few changes

--- server/index.js
+++ server/index.js
@@ -2,13 +2,50 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
 const authRoutes = require("./routes/auth");
 const complaintRoutes = require("./routes/complaint");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "X-XSRF-TOKEN"],
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  try {
+    const token = req.csrfToken();
+    res.cookie("XSRF-TOKEN", token, {
+      httpOnly: false,
+      secure: false, // Set to true when using HTTPS
+      sameSite: "Strict",
+    });
+  } catch (err) {}
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
